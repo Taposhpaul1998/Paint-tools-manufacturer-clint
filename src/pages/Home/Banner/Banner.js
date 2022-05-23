825e0dd402422e2f0ea98b6d015a2935caeccb20
Taposh paul <taposhp05@gamil.com>
2022-05-23 23:58:01 +0600

    create navber and banner

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen bg-[url('https://i.ibb.co/zHy66YN/painter-1937575-1280.jpg')]">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold text-white">Hero Paint Tools</h1>
+                    <p class="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;

fae5906e6e90a13e1506129a8c0bc18662caf834
Taposh paul <taposhp05@gamil.com>
2022-05-25 14:48:18 +0600

    create 404 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navber from './pages/Navber/Navber';
 import Home from './pages/Home/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Erorr from './pages/Erorr/Erorr';
 
 function App() {
   return (
-    <div>
+    <>
       <Navber></Navber>
-      <Home></Home>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/*" element={<Erorr></Erorr>}></Route>
+      </Routes>
+    </>
   );
 }
 
